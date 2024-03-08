ce0746ea5a286a61266cdfad389f3df1a8e206d1
Craig Francis <craig@craigfrancis.co.uk>
2024-03-08 11:35:34 +0000

    Cleanup undefined variables

--- reset/reset.sql
+++ reset/reset.sql
@@ -11,7 +11,7 @@ CREATE TABLE `user` (
 ) ;
 
 INSERT INTO user VALUES (1, 'admin', 'password', 'true', '');
-INSERT INTO user VALUES (13, 'made-you-look', 'hahaha', 'false', 'https://www.krang.org.uk/misc/scientist-100.jpg');
+INSERT INTO user VALUES (13, 'made-you-look', 'hahaha', 'false', 'https://craig.dev/misc/scientist-100.jpg');
 INSERT INTO user VALUES (666, 'evil', '666', 'false', './uploads/profile-pictures/666.js');
 INSERT INTO user VALUES (726, 'craig', 'password', 'false', './uploads/profile-pictures/726.jpg');
 INSERT INTO user VALUES (727, 'amy', 'yiecMK7eznbTwFuzDJEZXsYsycWaxX', 'false', './uploads/profile-pictures/727.jpg');
